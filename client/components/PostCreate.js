0502d763a5cd8772d104379dd4e4f6969183a6db
anilphiyak~ <anilphiyak617@gmail.com>
2023-01-08 11:42:19 +0530

    restructured react-app-client

---
+++ client/components/PostCreate.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PostCreate() {
+  
+  //random   
+//   github function
+    return (
+    <div>PostCreate</div>
+  )
+}
--- comments/index.js
+++ comments/index.js
@@ -4,17 +4,21 @@ const {randomBytes} =require('crypto')
 const app= express();
 app.use(express.json());
 
+const PORT=4001;
 const commentsByPostId ={};
 
-app.post('posts/:id/comments',(req,res)=>{
+app.get('/posts/:id/comments',(req,res)=>{
 
+    // console.log("get comments called");
     const postId=req.params.id;
-    express.send(commentsByPostId[req.params.id] || [] )
+    res.send(commentsByPostId[req.params.id] || [] )
 
 });
 
 
-app.post('posts/:id/comments',(req,res)=>{
+app.post('/posts/:id/comments',(req,res)=>{
+   
+
     const commentId= randomBytes(6).toString('hex');
     const {content} =req.body;
 
@@ -30,3 +34,7 @@ app.post('posts/:id/comments',(req,res)=>{
     res.status(201).send(comments);
 });
 
+
+app.listen(PORT,()=>{
+    console.log(`listening at port ${PORT}`);
+})
