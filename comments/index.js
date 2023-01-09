9835e1951d0a1756d267e53e668faeafa6e1319a
anilphiyak~ <anilphiyak617@gmail.com>
2023-01-09 22:29:24 +0530

    -comments-bug-fixed-cors

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,10 @@
 const express= require('express')
+const cors = require('cors')    
 const {randomBytes} =require('crypto')
 
 const app= express();
 app.use(express.json());
-
+app.use(cors());
 const PORT=4001;
 const commentsByPostId ={};
 
