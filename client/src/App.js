51b7fd5ebbf2a2bbe66eacfe3480524858870636
anilphiyak~ <anilphiyak617@gmail.com>
2023-01-09 22:34:17 +0530

    Add Post component and Tailwind CSS package to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
+import PostCreate from './components/PostCreate';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
       This is a blog App
+      <PostCreate/>
     </div>
   );
 }
 
 export default App;
+
+// what is export
