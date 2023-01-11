f7f9a329206ad0d3fecc00a82f097dcc5b843f57
anilphiyak~ <anilphiyak617@gmail.com>
2023-01-11 19:31:24 +0530

    updated fetch method

--- client/src/components/ListComments.js
+++ client/src/components/ListComments.js
@@ -2,22 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Comments from "./Comments";
 
-function ListComments({postId}) {
-
-    const [comments,setComments]=useState({});
-
-    const ENDPOINT=`http://localhost:4001/posts/:${postId}/comments`
-    const fetchComments= async ()=>{
-
-        console.log(postId);
-        const response=await axios.get(ENDPOINT);
-        setComments(response.data);
-        console.log(response.data);
-    }
-
-    useEffect(()=>{ 
-        fetchComments();
-    },[])
+function ListComments({postId,comments}) {
 
   return (
     <div>
--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -3,18 +3,20 @@ import CommentCreate from './CommentCreate';
 import ListComments from './ListComments';
 
 
-const PostCard = ({ title,id}) => (
-  <div className="m-2 bg-slate-300 post-card shadow-lg rounded-md p-4 ">
-    <h2 className="text-lg font-bold">{title}</h2>
-    <CommentCreate postId={id}/>
-    <ListComments postId={id}/>
-  </div>
-);
+const PostCard = ({ title,id,comments}) =>{
+  return (
+      <div className="m-2 bg-slate-300 post-card shadow-lg rounded-md p-4 ">
+      <h2 className="text-lg font-bold">{title}</h2>
+      <CommentCreate postId={id}/>
+      <ListComments comments={comments} postId={id}/>
+      </div>
+  );
+}
 
 
 export default function Posts({posts}) {
-
-  const renderedPosts= Object.values(posts).map(({title,id})=><PostCard title={title} id={id} key={id}/>)
+  
+  const renderedPosts= Object.values(posts).map(({title,comments,id})=><PostCard title={title} comments={comments} id={id} key={id}/>)
   
   return (
     <div className='m-auto w-3/5 flex flex-col justify-between m'>
