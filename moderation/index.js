f8bca819b2aaea7e71f0d02757afd441023c63b9
anilphiyak~ <anilphiyak617@gmail.com>
2023-03-02 10:16:37 +0530

    completed backedn

---
+++ moderation/index.js
@@ -0,0 +1,49 @@
+const express=require('express')
+const axios=require('axios')
+
+const PORT=4004
+const PORT_EVENT_BUS=4005
+const  app= express();
+
+app.use(express.json());
+
+// receives the event from event bus
+app.post('/events',async (req,res)=>{
+
+    const {type,data}=req.body;
+    if(type=='CommentCreated'){
+        const status= data.content.includes('orange')?'rejected':'approved';
+        console.log("comment created event recieved")
+
+            // emmiting the event to the event bus
+        try {
+            await setTimeout(async ()=>{
+                
+                await axios.post(`http://localhost:${PORT_EVENT_BUS}/events`,{
+                    type:'CommentModerated',
+                    data:{
+                        ...data,
+                        status:status
+                    }
+            }
+                );
+                
+                console.log("moderation-completed")},10000)
+        }
+        catch{
+            console.log("error in event bus");
+        }
+    }
+
+    res.send({});
+});
+
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
